21777e77bf24eecacee7cdd86288441eabae9d94
Sachin-8125 <devinewolf7@gmail.com>
2024-09-15 00:51:25 +0530

    started adding create service for tweets

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ const express =  require('express');
 const connect = require('./config/database');
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
+const Tweet = require('./models/tweet');
 
 app.listen(3000,async() => {
     console.log('Server started');
     await connect();
     console.log('Mongodb started');
+    const tweets = await Tweet.find({
+        content:  ["First tweet","my tweet"]
+        
+    })
+    console.log(tweets);
 });
---
+++ src/repository/index.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    TweetRepository: requestAnimationFrame('./tweet-repository')
+};
