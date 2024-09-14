7f9a24ada71d9b5a9a24c5738cbf732463312e06
Sachin-8125 <devinewolf7@gmail.com>
2024-09-14 23:47:41 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -9,12 +9,4 @@ app.listen(3000,async() => {
     console.log('Server started');
     await connect();
     console.log('Mongodb started');
-    // const tweet = await Tweet.create({
-    //     content: 'Third tweet'
-    // });
-    //const tweets = await Tweet.find({userEmail:'sachin@1.com'});
-   const tweetRepo = new TweetRepository();
-   const tweet  =await tweetRepo.create({content: 'With hooks now'});
-   console.log(tweet)
-
 });
